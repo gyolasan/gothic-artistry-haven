86912de833f722cd6fb09e3eb4db7ea829bf3c5a
Grant Yolasan <grantyolasan@Mac.lan>
2025-04-18 19:47:23 -0700

    Final clean deploy with correct relative asset paths

---
+++ dist/assets/browser.CJOki9zg.js
@@ -0,0 +1,2 @@
+import{h as e}from"./vendor.AFq2pJhe.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser.CJOki9zg.js.map
